49ea1a4ce361d7bffc41998cf51392363b699220
prorider <sebastian.ponceh@gmail.com>
2024-03-05 11:40:21 -0300

    Update importar base covid.py

--- importar base covid.py
+++ importar base covid.py
@@ -13,9 +13,9 @@ import sqlite3
 # In[12]:
 
 
-datos_muertes = pd.read_excel(r'C:\Users\poNze\OneDrive\Documentos\Proyecto SQl covid\CovidDeaths.xlsx')
-datos_vacunacion = pd.read_excel(r'C:\Users\poNze\OneDrive\Documentos\Proyecto SQl covid\Covidvacunacion.xlsx')
-conexion = sqlite3.connect(r'C:\Users\poNze\OneDrive\Documentos\Proyecto SQl covid\Covid.db')
+datos_muertes = pd.read_excel(r'directorio.xlsx')
+datos_vacunacion = pd.read_excel(r'directorio.xlsx')
+conexion = sqlite3.connect(r'directorio\Covid.db')
 
 datos_muertes.to_sql('Muertes',conexion, if_exists='replace',index=False)
 datos_vacunacion.to_sql('Vacunacion',conexion, if_exists='replace',index=False)
